9a7658a4ef60ba3eda819a661dd562c7dee0faef
Nastya <nastya12.11@yandex.ru>
2023-06-08 12:34:05 +0300

    Добавьте файлы проекта.

---
+++ Game/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Car_img = new System.Windows.Forms.PictureBox();
+            this.Road_img = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.Road_img2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Car_img)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Road_img)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Road_img2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Car_img
+            // 
+            this.Car_img.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Car_img.Image = global::Game.Properties.Resources.car;
+            this.Car_img.Location = new System.Drawing.Point(548, 510);
+            this.Car_img.Name = "Car_img";
+            this.Car_img.Size = new System.Drawing.Size(128, 128);
+            this.Car_img.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Car_img.TabIndex = 1;
+            this.Car_img.TabStop = false;
+            // 
+            // Road_img
+            // 
+            this.Road_img.Image = global::Game.Properties.Resources.Road;
+            this.Road_img.Location = new System.Drawing.Point(0, 0);
+            this.Road_img.Name = "Road_img";
+            this.Road_img.Size = new System.Drawing.Size(915, 650);
+            this.Road_img.TabIndex = 0;
+            this.Road_img.TabStop = false;
+            this.Road_img.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 20;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Road_img2
+            // 
+            this.Road_img2.Image = global::Game.Properties.Resources.Road;
+            this.Road_img2.Location = new System.Drawing.Point(0, -650);
+            this.Road_img2.Name = "Road_img2";
+            this.Road_img2.Size = new System.Drawing.Size(840, 650);
+            this.Road_img2.TabIndex = 2;
+            this.Road_img2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(840, 650);
+            this.Controls.Add(this.Car_img);
+            this.Controls.Add(this.Road_img);
+            this.Controls.Add(this.Road_img2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.Car_img)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Road_img)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Road_img2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Road_img;
+        private System.Windows.Forms.PictureBox Car_img;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.PictureBox Road_img2;
+    }
+}
+
---
+++ Game/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game
+{
+    public partial class Form1 : Form
+    {
+        private Point pos; // то где впервый раз нажали мышкой
+        private bool dragging; // передвигаем окно или нет
+        public Form1()
+        {
+            InitializeComponent();
+            Road_img2.MouseDown += MouseClickDown;
+            Road_img2.MouseDown += MouseClickUp;
+            Road_img2.MouseDown += MouseClickMove;
+            Road_img.MouseDown += MouseClickDown;
+            Road_img.MouseUp += MouseClickUp;
+            Road_img.MouseMove += MouseClickMove;
+
+        }
+
+        private void MouseClickDown(object sender, MouseEventArgs e)
+        {
+            dragging = true;
+            pos.X = e.X;
+            pos.Y = e.Y;
+        }
+        private void MouseClickUp(object sender, MouseEventArgs e)
+        {
+            dragging = false;
+        }
+        private void MouseClickMove(object sender, MouseEventArgs e)
+        {
+            if (dragging)
+            {
+                Point currPoint = PointToScreen(new Point(e.X, e.Y));
+                this.Location = new Point(currPoint.X - pos.X, currPoint.Y - pos.Y + Road_img.Top);
+            }
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Escape) { 
+                this.Close();
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            int speed = 10;
+            Road_img2.Top += speed;
+            Road_img.Top += speed;
+            if (Road_img.Top >= 650) {
+                Road_img2.Top = -640;
+                Road_img.Top = 0;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int speedCar = 10;
+            if((e.KeyCode == Keys.Left || e.KeyCode == Keys.A) && Car_img.Left >= 150 ) {
+                Car_img.Left -= speedCar;
+            }
+            else if ((e.KeyCode == Keys.D || e.KeyCode == Keys.Right) && Car_img.Right <= 700)
+            {
+                Car_img.Left += speedCar;
+            }
+        }
+    }
+}
